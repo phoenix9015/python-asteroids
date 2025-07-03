e4d219be8f58de2394fe8c1080a3ef6de6093e8c
lightningbow7391 <lightningbow7391@protonmail.ch>
2025-07-02 21:32:21 -0400

    Initial pygame setup

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+from constants import *
+import pygame
+
+
 def main():
-    pass
+    # pygame setup
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        # Poll for events
+        for event in pygame.event.get():
+            # User closed window
+            if event.type == pygame.QUIT:
+                running = False
+        
+        screen.fill("black")
+        # ---------------------------------------------
+        
+
+        # ---------------------------------------------
+        # Update graphics
+        pygame.display.flip()
+        clock.tick(60)
+
+
+    pygame.quit()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
